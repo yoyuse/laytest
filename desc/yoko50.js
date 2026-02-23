03eef7b16ab94bca27db6a78c1aa5eb2b7896467
yoyuse <yoyuse@gmail.com>
2026-02-23 13:50:00 +0900

    yoko50

---
+++ desc/yoko50.js
@@ -0,0 +1,7 @@
+descs["yoko50"] = new Desc(
+  "yoko50", "横50音",
+  `<h2>横50音</h2>
+<p>
+<a href="http://jgrammar.life.coocan.jp/ja/tools/imekeys.htm#Yoko50">横五十音配列 (横50音配列)</a>
+</p>`,
+    "img/kb-yoko50-hhkb.svg");
