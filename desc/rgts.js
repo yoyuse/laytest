747d6adf8b770dd7f3899c4d79e095bfd12c8b4b
yoyuse <yoyuse@gmail.com>
2026-02-28 23:40:00 +0900

    link to RGTs

--- desc/rgts.js
+++ desc/rgts.js
@@ -2,7 +2,9 @@ descs["rgts"] = new Desc(
     "rgts", "RGTs",
     `<h2>RGTs</h2>
 <p>
-<a href="https://qiita.com/dulunoj/items/95ebe5e40031183c5eea">RGT</a> の私家版。オリジナルとの主な違いは以下:
+<a href="https://qiita.com/dulunoj/items/95ebe5e40031183c5eea">RGT</a> の
+<a href="https://github.com/yoyuse/kkh/blob/4073b37c9f54c99096d2ad1b3446806420d77bd3/extra/rgts.md">私家版</a>
+。オリジナルとの主な違いは以下:
 </p>
 <ul>
 <li>&ldquo;y&rdquo; に『き』を割り当て</li>
